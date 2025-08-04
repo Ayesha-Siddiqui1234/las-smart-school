d578e5925e184a781b8808b8c97ca335e7c1aa3f
Ayesha Siddiqui <155626270+Ayesha-Siddiqui1234@users.noreply.github.com>
2025-08-04 22:19:17 +0500

    Update Main2.jsx

--- src/components/Main2.jsx
+++ src/components/Main2.jsx
@@ -19,7 +19,7 @@ function Main2() {
             <h1 className='font-bold text-3xl lg:text-5xl mb-4 text-white'>About LAS</h1>
             <p className='text-white text-lg lg:text-xl'>
               
-               LAS Smart School is dedicated to providing quality education and holistic development. The school emphasizes academic excellence, critical thinking, and ethical values. It boasts modern infrastructure, including advanced ICT labs, spacious classrooms, Audio Video Hall, mobile and web base student portal systemand Stem Education. LAS aims to empower students with knowledge and skills for a successful future.
+               LAS Smart School is dedicated to providing quality education and holistic development. The school emphasizes academic excellence, critical thinking, and ethical values. It boasts modern infrastructure, including advanced ICT labs, spacious classrooms, Audio Video Hall, mobile and web base student portal systemand Stem Education. 
             </p>
 <button 
 className="bg-gradient-to-r from-black to-black hover:to-purple-900 p-2 lg:px-6 lg:py-3 rounded-lg text-lg font-bold mt-5 lg:mb-5 text-light transition duration-300"
